aa6919194122729fca1f7457b70119364458d1e7
Hrishikesh Ballal <hballal@gmail.com>
2023-05-23 06:41:46 +0100

    Added design shadow analysis

--- notifications_helper.py
+++ notifications_helper.py
@@ -8,7 +8,7 @@ def notify_shadow_complete(job, connection, result, *args, **kwargs):
     job_id = job.id  
     app = create_app()      
     with app.app_context():
-        sse.publish({"shadow_key": job_id}, type='diagram_shadow_success')
+        sse.publish({"shadow_key": job_id}, type='shadow_generation_success')
         
 def shadow_generation_failure(job, connection, type, value, traceback):
     print('Job with %s failed..'% str(job.id))
