5a44cdc43b1f74ea0d2221ab0c60fbbdbee1f961
hrishiballal <hrishikeshballal@yahoo.com>
2023-06-07 15:42:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    serviceurl = "http://local.test:8000/api/v1/"    
+    serviceurl = "https://www.geodesignhub.com/api/v1/"    
     REDIS_URL= os.environ.get("REDIS_URL", 'redis://localhost:6379')
     ROADS_URL = os.environ.get("ROADS_URL", None)
 
