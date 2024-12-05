e218792fafed0e1a0a31bf2b733ef515c05ee30c
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-12-05 13:34:54 +0000

    Update conn.py

--- conn.py
+++ conn.py
@@ -1,10 +1,13 @@
-import redis
+
 from os import environ as env
+from redis import ConnectionPool, Redis
+
 import logging
 logger = logging.getLogger("local-climate-response")
 
 def get_redis():
     # A method to get the redis instance and is used globally
-    redis_url = env.get('REDIS_URL','redis://local.test:6379')    
-    r = redis.from_url(redis_url)
-    return r
+    redis_url = env.get("REDIS_URL", "redis://local.test:6379")
+    connection_pool = ConnectionPool.from_url(redis_url)
+    redis = Redis(connection_pool=connection_pool, ssl_cert_reqs=None)
+    return redis
