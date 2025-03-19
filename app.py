f80d537f752c899064116ce6c0382b394695b392
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2025-03-19 17:38:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -604,23 +604,23 @@ def draw_trees_view():
     )
 
 
-def has_no_empty_params(rule):
-    defaults = rule.defaults if rule.defaults is not None else ()
-    arguments = rule.arguments if rule.arguments is not None else ()
-    return len(defaults) >= len(arguments)
+# def has_no_empty_params(rule):
+#     defaults = rule.defaults if rule.defaults is not None else ()
+#     arguments = rule.arguments if rule.arguments is not None else ()
+#     return len(defaults) >= len(arguments)
 
 
-@app.route("/site-map")
-def site_map():
-    links = []
-    for rule in app.url_map.iter_rules():
-        # Filter out rules we can't navigate to in a browser
-        # and rules that require parameters
-        if "GET" in rule.methods and has_no_empty_params(rule):
-            url = url_for(rule.endpoint, **(rule.defaults or {}))
-            links.append((url, rule.endpoint))
-    return jsonify(links)
-    # links is now a list of url, endpoint tuples
+# @app.route("/site-map")
+# def site_map():
+#     links = []
+#     for rule in app.url_map.iter_rules():
+#         # Filter out rules we can't navigate to in a browser
+#         # and rules that require parameters
+#         if "GET" in rule.methods and has_no_empty_params(rule):
+#             url = url_for(rule.endpoint, **(rule.defaults or {}))
+#             links.append((url, rule.endpoint))
+#     return jsonify(links)
+#     # links is now a list of url, endpoint tuples
 
 
 if __name__ == "__main__":
