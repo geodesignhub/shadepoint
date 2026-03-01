14197f4fc444d563bcc45672bc45976667765676
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2026-03-01 15:56:06 +0000

    added prestart

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -14,14 +14,16 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.register_blueprint(nbsapi.nbsapi_blueprint)
-
     app.config.from_object(config_class)
-    babel = Babel(app)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    babel = Babel(app)
     Bootstrap5(app)
     CSRFProtect(app)
     db.init_app(app)
     migrate.init_app(app, db)
+
+    from .nbsapi import nbsapi_blueprint
+    app.register_blueprint(nbsapi_blueprint)
 
     return app, babel
